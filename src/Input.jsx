6f670cc45f81d04a8207601c1fac490bf77d02c4
GUchechi <godswilluchendu7@gmail.com>
2022-09-12 13:53:06 +0100

    change text color with toggle text

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
+import colorNames from 'colornames'
 
-const Input = ({colorValue, setColorValue, setHexValue}) => {
+const Input = ({
+  colorValue, setColorValue, setHexValue, isDarkText, setIsDarkText
+}) => {
   return (
     <form onSubmit={(e) => e.preventDefault()}>
         <label>Add color name</label>
         <input
-        autoFocus
-         type="text"
-         placeholder='Add a new color'
-         required
-         value={colorValue}
-         onChange={(e) => {
-          setColorValue(e.target.value);
-          setHexValue()
+            autoFocus
+            type="text"
+            placeholder='Add a new color'
+            required
+            value={colorValue}
+            onChange={(e) => {
+              setColorValue(e.target.value);
+              setHexValue(colorNames(e.target.value));
           }}       
           />
+        <button
+          onClick={(e) => {
+            setIsDarkText(!isDarkText)
+          }}
+        >Toggle Text</button>
+
     </form>
   )
 }
